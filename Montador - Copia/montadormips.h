3691d08fd9b35a44acf877fcef19bb481b50c695
ruanformigoni <formigoni@outlook.com>
2017-05-10 02:46:33 -0300

    Ver. 0.2.1
    - Revisão de arquivos

--- Montador - Copia/montadormips.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void Assembly_Leitura (FILE** Entrada_asm, char* instrucao, char* destino, char* parametro1, char* parametro2);
-void Assembly_Montagem(FILE** Saida_asm, char* instrucao, char* destino, char* parametro1, char* parametro2);
-void Registrador_Maquina(char* entrada, char* saida);
-
